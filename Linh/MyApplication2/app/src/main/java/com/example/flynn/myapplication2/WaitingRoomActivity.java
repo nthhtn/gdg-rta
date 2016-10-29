f75b908ba9a9614f4bc222029deb2b49507a1496
Flynn3094 <linhnguyen3094@gmail.com>
2016-10-29 11:18:19 +0700

    WaitingRoomActivity

--- Linh/MyApplication2/app/src/main/java/com/example/flynn/myapplication2/WaitingRoomActivity.java
+++ Linh/MyApplication2/app/src/main/java/com/example/flynn/myapplication2/WaitingRoomActivity.java
@@ -2,6 +2,10 @@ package com.example.flynn.myapplication2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class WaitingRoomActivity extends Activity {
 
@@ -9,5 +13,34 @@ public class WaitingRoomActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting_room);
+//        init();
+    }
+
+    public void init(){
+        TableLayout table = (TableLayout) findViewById(R.id.tableWaiting);
+
+
+        for (int i = 0; i <6; i++) {
+
+            TableRow row= (TableRow) findViewById(R.id.tableRow);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+
+            TextView idTV = new TextView(this);
+            TextView playerTV = new TextView(this);
+            TextView teamTV = new TextView(this);
+            TextView statusTV = new TextView(this);
+
+            idTV.setText("1");
+            playerTV.setText("Conan kun");
+            teamTV.setText("Blue");
+            statusTV.setText("Ready");
+
+            row.addView(idTV);
+            row.addView(playerTV);
+            row.addView(teamTV);
+            row.addView(statusTV);
+            table.addView(row, i);
+        }
     }
 }
