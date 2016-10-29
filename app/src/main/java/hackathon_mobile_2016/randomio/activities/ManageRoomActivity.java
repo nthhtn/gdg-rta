5ee2815041f728711553ea551c6be44f0dcc93fb
Nguyen Duc Tri <ductricse@gmail.com>
2016-10-30 03:03:57 +0700

    ok

--- app/src/main/java/hackathon_mobile_2016/randomio/activities/ManageRoomActivity.java
+++ app/src/main/java/hackathon_mobile_2016/randomio/activities/ManageRoomActivity.java
@@ -1,8 +1,11 @@
 package hackathon_mobile_2016.randomio.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import hackathon_mobile_2016.randomio.R;
 
@@ -12,5 +15,21 @@ public class ManageRoomActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_room);
+
+        Button btnBegin = (Button) findViewById(R.id.btnBegin);
+        btnBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MatchActivity.class);
+                intent.putExtra("isHost", "true");
+                intent.putExtra("roomId", "-KVFHhZrr2qZ1d-rNnDt");
+                intent.putExtra("team", "2");
+                intent.putExtra("mode", "1");
+                intent.putExtra("maxNumber", "10");
+                intent.putExtra("playerId","2");
+                intent.putExtra("playerName", "ductri");
+                startActivity(intent);
+            }
+        });
     }
 }
