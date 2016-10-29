6e2d7c7a656e2d14012755ddabb9bd1e5759a371
Nguyen Duc Tri <ductricse@gmail.com>
2016-10-29 12:42:59 +0700

    temp

--- app/src/main/java/hackathon_mobile_2016/randomio/WaitingRoom.java
+++ app/src/main/java/hackathon_mobile_2016/randomio/WaitingRoom.java
@@ -2,6 +2,9 @@ package hackathon_mobile_2016.randomio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class WaitingRoom extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class WaitingRoom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting_room);
+    }
+
+    public void init(){
+        TableLayout table = (TableLayout) findViewById(R.id.tableWaiting);
+
+        for (int i = 0; i <6; i++) {
+
+            TableRow row= (TableRow) findViewById(R.id.tableRow);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+
+            TextView idTV = new TextView(this);
+            TextView playerTV = new TextView(this);
+            TextView teamTV = new TextView(this);
+            TextView statusTV = new TextView(this);
+
+            idTV.setText("1");
+            playerTV.setText("Conan kun");
+            teamTV.setText("Blue");
+            statusTV.setText("Ready");
+
+            row.addView(idTV);
+            row.addView(playerTV);
+            row.addView(teamTV);
+            row.addView(statusTV);
+            table.addView(row, i);
+        }
     }
 }
