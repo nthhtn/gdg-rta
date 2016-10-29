c879885f120c3cc59787e4e60e72178344f830be
Flynn3094 <linhnguyen3094@gmail.com>
2016-10-29 20:46:15 +0700

    master submit

---
+++ Linh/MyApplication2/app/src/main/java/com/example/flynn/myapplication2/FindRoomActivity.java
@@ -0,0 +1,19 @@
+package com.example.flynn.myapplication2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FindRoomActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_room);
+        init();
+    }
+
+    public void init() {
+
+
+    }
+}
